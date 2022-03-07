581f0cc78b9db82951cf60eea041c226ac4b3190
wansongtao <18101837209@163.com>
2022-03-07 15:38:04 +0800

    feat:完成笔记列表页的数据显示和滚动加载功能

--- pages/noteList/index.js
+++ pages/noteList/index.js
@@ -1,4 +1,7 @@
 // pages/noteList/index.js
+import { separator, noteFilePrev} from '../../config/index'
+import { setFileName, readFile, dataFormatConversion } from '../../utils/util'
+
 Page({
 
   /**
@@ -6,20 +9,61 @@ Page({
    */
   data: {
     searchVal: '',
-    list: [{
-        id: 324133424,
-        title: '不过是大梦一场空',
-        content: '不过是孤影照惊鸿，不过是白驹之过一场梦。不过是孤影照惊鸿，不过是白驹之过一场梦。不过是孤影照惊鸿，不过是白驹之过一场梦。不过是孤影照惊鸿，不过是白驹之过一场梦。不过是孤影照惊鸿，不过是白驹之过一场梦。不过是孤影照惊鸿，不过是白驹之过一场梦。不过是孤影照惊鸿，不过是白驹之过一场梦。',
-        createTime: '2022/03/07 12:12:12'
-      }
-    ]
+    list: []
   },
-
+  global: {
+    allData: [],
+    pageSize: 6
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.initData()
+  },
+  initData() {
+    const list = this.getNoteData()
 
+    if (!list.length) {
+      this.global.allData = []
+      this.setData({
+        list: []
+      })
+      return
+    }
+
+    list.sort((a, b) => {
+      if (a.createTime > b.createTime) {
+        return -1
+      }
+      if (a.createTime < b.createTime) {
+        return 1
+      }
+      return 0
+    })
+
+    this.global.allData = list
+    const showList = list.slice(0, this.global.pageSize)
+
+    this.setData({
+      list: showList
+    })
+  },
+  /**
+   * @description 获取某月的笔记数据
+   * @param {string} date 年月，格式：yyyymm
+   * @returns {Object[]} 成功返回数据列表，失败返回空数组
+   */
+  getNoteData(date) {
+    const fileName = setFileName(noteFilePrev, date)
+    const text = readFile(fileName)
+
+    if (!text) {
+      return []
+    }
+
+    const list = dataFormatConversion(text, separator)
+    return list.filter((item) => item.isDelete !== 1)
   },
   onSearch() {
 
@@ -29,8 +73,21 @@ Page({
       searchVal: ''
     })
   },
-  onLoad() {
+  onLoadMore() {
+    const showList = this.data.list
+    const allData = this.global.allData
 
+    if (showList.length >= allData.length) {
+      return
+    }
+
+    const newList = allData.slice(showList.length, showList.length + this.global.pageSize)
+
+    showList.push(...newList)
+
+    this.setData({
+      list: showList
+    })
   },
   onLook(e) {
     const id = e.target.dataset.id
