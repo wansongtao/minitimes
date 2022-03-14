34d814d6d83e3339830ae227059abb01dae66c61
wansongtao <18101837209@163.com>
2022-03-14 19:33:35 +0800

    feat:搜索框防抖处理

--- pages/noteSearch/index.js
+++ pages/noteSearch/index.js
@@ -5,16 +5,28 @@ Page({
   data: {
     searchVal: ''
   },
+  global: {
+    timer: null,
+    spacing: 200
+  },
   onLoad: function (options) {
 
   },
   onChange(e) {
-    const value = e.detail
-    this.setData({
-      searchVal: value
-    })
+    const global = this.global
+    if (global.timer) {
+      clearTimeout(global.timer)
+    }
+
+    global.timer = setTimeout(() => {
+      const value = e.detail
+
+      this.setData({
+        searchVal: value
+      })
+    }, global.spacing);
   },
   onSearch() {
-    
+
   }
 })
