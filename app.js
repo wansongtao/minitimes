e4ae3cbbdb8ff096f201e7235db0d9682e5ebd54
wansongtao <18101837209@163.com>
2022-03-16 17:31:59 +0800

    perf:迁移上个版本的数据

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 // app.js
 import {
-  updateMiniprogram
+  updateMiniprogram,
+  renameFile
 } from './utils/util'
+import { getFileList, mkdirDirectory } from './utils/file'
+import { noteDirectory, planDirectory } from './config/index'
 
 App({
   onLaunch() {
     updateMiniprogram()
+
+    const files = getFileList()
+
+    files.forEach((item) => {
+      if (item.indexOf('.txt') === -1) {
+        return
+      }
+
+      if (item.indexOf('plans_') !== -1) {
+        mkdirDirectory(planDirectory)
+        renameFile(item, 'plan/' + item)
+        return
+      }
+
+      if (item.indexOf('notes_') !== -1) {
+        mkdirDirectory(noteDirectory)
+        renameFile(item, 'note/' + item)
+        return
+      }
+    })
   },
   globalData: {
     isUpdatePlan: false, // 是否需要更新首页的计划数据
